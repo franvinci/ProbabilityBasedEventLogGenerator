5e7bb5fde25324adabb32c047fa6f0f7a6d7c57f
FraVinci <82420026+franvinci@users.noreply.github.com>
2024-12-11 10:58:11 +0100

    Add Distance Attributes

---
+++ distance_utils.py
@@ -0,0 +1,46 @@
+from scipy.stats import wasserstein_distance
+from collections import Counter
+import pandas as pd
+import numpy as np
+
+
+def emd_categ(a: list, b: list):
+    
+    a_counts = Counter(a)
+    b_counts = Counter(b)
+
+    unique_elements = sorted(set(a + b))
+
+    a_hist = [a_counts[elem] for elem in unique_elements]
+    b_hist = [b_counts[elem] for elem in unique_elements]
+
+    mapping = {elem: i for i, elem in enumerate(unique_elements)}
+    numeric_positions = [mapping[elem] for elem in unique_elements]
+
+    distance = wasserstein_distance(numeric_positions, numeric_positions, u_weights=a_hist, v_weights=b_hist)
+
+    return distance
+
+
+def emd_attributes(df_real: pd.DataFrame, df_sim:pd.DataFrame, attr_names=[], fillna=True):
+    
+    for a in attr_names:
+        if a not in df_real.columns:
+            raise Exception(f"{a} not in df_real columns")
+        if a not in df_sim.columns:
+            raise Exception(f"{a} not in df_sim columns")
+        
+    emds = []
+    for a in attr_names:
+        if df_real[a].dtype == float:
+            if fillna:
+                emds.append(wasserstein_distance(df_real[a].fillna(df_real[a].mean()), df_sim[a].fillna(df_sim[a].mean())))
+            else:
+                emds.append(wasserstein_distance(df_real[a].dropna(), df_sim[a].dropna()))
+        else:
+            if fillna:
+                emds.append(emd_categ(list(df_real[a].fillna("N/A")), list(df_sim[a].fillna("N/A"))))
+            else:
+                emds.append(emd_categ(list(df_real[a].dropna(), list(df_sim[a].dropna()))))
+
+    return np.mean(emds)
